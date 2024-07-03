ca31bda5e2736a3696802bf39fba7e0fcdaade41
Jordane Brosset <jordane.brosset@gmail.com>
2024-07-03 08:27:36 -0600

    Changing cache

--- src/app/api/booked-dates/route.ts
+++ src/app/api/booked-dates/route.ts
@@ -1,5 +1,6 @@
 import prisma from "../../../lib/prisma"; // Adjust the import based on your actual file structure
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
     const bookedDates = await prisma.bookedDate.findMany();
--- src/app/api/getprices/route.ts
+++ src/app/api/getprices/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2024-04-10",
 });
+export const dynamic = "force-dynamic";
 export async function GET(req: Request) {
     const prices = await stripe.prices.list();
 
